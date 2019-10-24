a67741a8758115604e803fa6c4836cd5cb2e6f2d
Renthuby <56352170+Renthuby@users.noreply.github.com>
2019-10-24 21:14:39 +0700

    Учусь :)

--- app/src/main/java/com/example/myapplication/Array.java
+++ app/src/main/java/com/example/myapplication/Array.java
@@ -37,9 +37,10 @@ public class Array {
         System.out.println(M+" " + N);
 
         for (int j = 0; j < M; j++) {
-            for (int i = N-1; i >= 0; i--) {
-                System.out.print(a[i][j]+" ");
+            for (int i = N - 1; i >= 0; i--) {
+                System.out.print(a[i][j] + " ");
             }
             System.out.println();
+        }
     }
 }
